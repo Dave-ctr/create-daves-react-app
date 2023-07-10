c6dc459c8e2dff81f9bf9d116ffe612425092bd9
Dave-ctr <83482840+Dave-ctr@users.noreply.github.com>
2023-07-11 10:42:55 +1200

    This commit is my react app when I've just started incorporating Lists and Keys - Comments are R.E buttons and clicks and incorporate useState and useEffect hooks.
    I also learned the value of considered commit comments - the standout for me being referable code that is not current but still of value...
    
    Thank you Sofia

--- src/Content.js
+++ src/Content.js
@@ -1,46 +1,75 @@
 import React, { useState } from "react";
 
-function Content()
+function Content = () =>
 {
-  // eslint-disable-next-line no-unused-vars
-  const [ name, setName ] = useState( "Dave" );
+  const [ items, setItems ] = useState( [
+    {
+      id: 1,
+      checked: false,
+      item: "Blah"
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Blah Blah"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Blah Blah Blah"
+    }
+  ] );
 
-  // eslint-disable-next-line no-unused-vars
-  const [ count, setCount ] = useState( 0 )
 
-  // eslint-disable-next-line no-lone-blocks
-  {/*const handleNameChange = () =>
-  {
-    const names = [ "Bob", "Dave", "Bill", ]
-    const int = Math.floor( Math.random() * 3 )
-    return setName( names[ int ] )
-  }
 
-  const handleClick = () =>
-  {
-    setCount( count + 1 )
-    console.log( "Clicked It Count", count )
-  }
-  const handleClick1 = ( name ) =>
-  {
-    console.log( `${ name } Was Clicked` )
-  }
-  const handleClick2 = ( e ) =>
-  {
-    console.log( `${ e.target.innerText } Was Clicked` )
-  }*/}
+
+
 
   return (
     <main>
-      {/*} <p onDoubleClick={ handleClick }>
+
+
+    </main>
+  )
+}
+
+export default Content
+
+/*
+    eslint-disable-next-line no-unused-vars
+   const [ name, setName ] = useState( "Dave" );
+ 
+    eslint-disable-next-line no-unused-vars
+   const [ count, setCount ] = useState( 0 )
+ 
+    eslint-disable-next-line no-lone-blocks
+   const handleNameChange = () =>
+   {
+     const names = [ "Bob", "Dave", "Bill", ]
+     const int = Math.floor( Math.random() * 3 )
+     return setName( names[ int ] )
+   }
+ 
+   const handleClick = () =>
+   {
+     setCount( count + 1 )
+     console.log( "Clicked It Count", count )
+   }
+   const handleClick1 = ( name ) =>
+   {
+     console.log( `${ name } Was Clicked` )
+   }
+   const handleClick2 = ( e ) =>
+   {
+     console.log( `${ e.target.innerText } Was Clicked` )
+   }
+ */
+
+{/*} <p onDoubleClick={ handleClick }>
         Hello { name }!
       </p>
       <button onClick={ handleNameChange }>Change Name</button>
       <button onClick={ handleClick }>Count</button>
       <button onClick={ () => handleClick1( 'Dave' ) }>Dave - Click It </button>
-  <button onClick={ ( e ) => handleClick2( e ) }>Event - Click It </button>*/}
-    </main>
-  )
-}
-
-export default Content;
+  <button onClick={ ( e ) => handleClick2( e ) }>Event - Click It </button>
+*/};
