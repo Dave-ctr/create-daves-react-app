29cfe8a314371dec2535b235b1b51598d830d767
Dave-ctr <83482840+Dave-ctr@users.noreply.github.com>
2023-07-17 10:45:18 +1200

    Successful completion of Lists and Keys!! Next Up - Props and Prop Drilling. Everything rendering as expected so far, my original error quite possibly came from /* const handleCheck = ( id ) =>   {     const listItems = items.map( ( item ) => item.id === id ? { ...item, checked: !item.checked } : item );     setItems( listItems );     localStorage.setItem( 'stufflist', JSON.stringify( listItems ) );   }   */   my unseen omission of "id" post "===" and pre "?".   Silly mistake but will be interesting to see if that's what caused everything else to fail downstream earlier as I move forward with destructuring my Content.js with Props and Prop Drilling.

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
-function Content = () =>
+const Content = () =>
 {
   const [ items, setItems ] = useState( [
     {
@@ -20,56 +21,51 @@ function Content = () =>
     }
   ] );
 
+  const handleCheck = ( id ) =>
+  {
+    const listItems = items.map( ( item ) => item.id === id ? { ...item, checked: !item.checked } : item );
+    setItems( listItems );
+    localStorage.setItem( 'stufflist', JSON.stringify( listItems ) );
+  }
 
-
-
-
+  const handleDelete = ( id ) =>
+  {
+    const listItems = items.filter( ( item ) => item.id !== id );
+    setItems( listItems );
+    localStorage.setItem( 'stufflist', JSON.stringify( listItems ) );
+  }
 
   return (
     <main>
-
-
-    </main>
+      { items.length ? (
+        <ul>
+          { items.map( ( item ) => (
+            <li className="item" key={ item.id }>
+              <input
+                type="checkbox"
+                onChange={ () => handleCheck( item.id ) }
+                checked={ item.checked }
+              />
+              <label
+                style={ ( item.checked ) ? { textDecoration: 'line-through' } : null }
+                onDoubleClick={ () => handleCheck( item.id ) }>
+                { item.item }
+              </label>
+              <FaTrashAlt
+                onClick={ () => handleDelete( item.id ) }
+                role="button"
+                tabIndex='0'
+                aria-label=""
+              />
+            </li>
+          ) ) }
+        </ul>
+      ) : (
+        <p style={ { marginTop: '2rem' } }>Nuk List</p>
+      ) }
+    </main >
   )
 }
 
 export default Content
 
-/*
-    eslint-disable-next-line no-unused-vars
-   const [ name, setName ] = useState( "Dave" );
- 
-    eslint-disable-next-line no-unused-vars
-   const [ count, setCount ] = useState( 0 )
- 
-    eslint-disable-next-line no-lone-blocks
-   const handleNameChange = () =>
-   {
-     const names = [ "Bob", "Dave", "Bill", ]
-     const int = Math.floor( Math.random() * 3 )
-     return setName( names[ int ] )
-   }
- 
-   const handleClick = () =>
-   {
-     setCount( count + 1 )
-     console.log( "Clicked It Count", count )
-   }
-   const handleClick1 = ( name ) =>
-   {
-     console.log( `${ name } Was Clicked` )
-   }
-   const handleClick2 = ( e ) =>
-   {
-     console.log( `${ e.target.innerText } Was Clicked` )
-   }
- */
-
-{/*} <p onDoubleClick={ handleClick }>
-        Hello { name }!
-      </p>
-      <button onClick={ handleNameChange }>Change Name</button>
-      <button onClick={ handleClick }>Count</button>
-      <button onClick={ () => handleClick1( 'Dave' ) }>Dave - Click It </button>
-  <button onClick={ ( e ) => handleClick2( e ) }>Event - Click It </button>
-*/};
