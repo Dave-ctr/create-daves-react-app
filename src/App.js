503bc6a1cdf9b517d16a8c0c22fe6d365b488926
Dave-ctr <83482840+Dave-ctr@users.noreply.github.com>
2023-07-17 13:39:57 +1200

    whoop whoop

--- src/App.js
+++ src/App.js
@@ -1,45 +1,65 @@
 import Header from './Header';
+import SearchItem from './SearchItem'; import AddItem from "./AddItem";
 import Content from './Content';
 import Footer from "./Footer";
 import { useState } from "react";
 
+
 function App()
 {
-  const [ items, setItems ] = useState( [
-    {
-      id: 1,
-      checked: false,
-      item: "Blah"
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Blah Blah"
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Blah Blah Blah"
-    }
-  ] );
+  const [ items, setItems ] = useState( JSON.parse( localStorage.getItem( "stufflist" ) ) );
+
+  const [ newItem, setNewItem ] = useState( '' )
+
+  const [ search, setSearch ] = useState( '' )
+
+  const setAndSaveItems = ( newItems ) =>
+  {
+    setItems( newItems );
+    localStorage.setItem( 'stufflist', JSON.stringify( newItems ) );
+  }
+
+  const addItem = ( item ) =>
+  {
+    const id = items.length ? items[ items.length - 1 ].id + 1 : 1;
+    const myNewItem = { id, checked: false, item }
+    const listItems = [ ...items, myNewItem ]
+    setAndSaveItems( listItems );
+  }
+
 
   const handleCheck = ( id ) =>
   {
     const listItems = items.map( ( item ) => item.id === id ? { ...item, checked: !item.checked } : item );
-    setItems( listItems );
-    localStorage.setItem( 'stufflist', JSON.stringify( listItems ) );
+    setAndSaveItems( listItems );
   }
 
   const handleDelete = ( id ) =>
   {
     const listItems = items.filter( ( item ) => item.id !== id );
-    setItems( listItems );
-    localStorage.setItem( 'stufflist', JSON.stringify( listItems ) );
+    setAndSaveItems( listItems );
+  }
+
+  const handleSubmit = ( e ) =>
+  {
+    e.preventDefault()
+    if ( !newItem ) return;
+    addItem( newItem );
+    setNewItem( '' );
   }
 
   return (
     <div className="App">
       <Header title="List" />
+      <SearchItem
+        search={ search }
+        setSearch={ setSearch }
+      />
+      <AddItem
+        newItem={ newItem }
+        setNewItem={ setNewItem }
+        handleSubmit={ handleSubmit }
+      />
       <Content
         items={ items }
         handleCheck={ handleCheck }
